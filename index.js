46350c55245941299a7679550d083397ce2f0c59
Nigibob <nelsonat@laposte.net>
2019-11-17 18:42:02 +0100

    JS + readme commit

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
-const server = http.createServer(serverHandle);
+const http = require('http');
+const handles = require('./handles');
+const server = http.createServer(handles.serverHandle);
 server.listen(8080)
